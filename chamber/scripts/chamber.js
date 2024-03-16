6e8c2a622ede7d6206e4b4fd30f7e28b01fd0772
jgcnsny <thegreathappyvirus@gmail.com>
2024-03-16 21:33:39 +0800

    discover

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -18,18 +18,13 @@ hamButton.addEventListener('click', () => {
 });
 
 document.addEventListener("DOMContentLoaded", function() {
-    // Get the current date in milliseconds
     var currentDate = Date.now();
-
-    // Retrieve the last visit date from localStorage
     var lastVisitDate = localStorage.getItem("lastVisitDate");
 
     if (lastVisitDate) {
-        // Calculate the difference in milliseconds between current date and last visit date
         var differenceInMs = currentDate - parseInt(lastVisitDate);
         var differenceInDays = Math.floor(differenceInMs / (1000 * 60 * 60 * 24));
 
-        // Update the message based on the difference in days
         var message = "";
         if (differenceInDays === 0) {
             message = "Back so soon! Awesome!";
@@ -37,14 +32,13 @@ document.addEventListener("DOMContentLoaded", function() {
             message = "You last visited " + (differenceInDays === 1 ? "1 day" : differenceInDays + " days") + " ago.";
         }
     } else {
-        // First visit
+
         message = "Welcome! Let us know if you have any questions.";
     }
 
-    // Display the message in the sidebar content area
+
     var sidebarContent = document.querySelector("#dateVisit");
     sidebarContent.textContent = message;
 
-    // Store the current date as the last visit date in localStorage
     localStorage.setItem("lastVisitDate", currentDate.toString());
 });
