831fdba0197300eb72c8212a7ee2211db406473b
jgcnsny <thegreathappyvirus@gmail.com>
2024-03-24 01:22:12 +0800

    join

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -17,28 +17,13 @@ hamButton.addEventListener('click', () => {
 	hamButton.classList.toggle('open');
 });
 
-document.addEventListener("DOMContentLoaded", function() {
-    var currentDate = Date.now();
-    var lastVisitDate = localStorage.getItem("lastVisitDate");
+function getCurrentTimestamp() {
+    return new Date().toISOString();
+}
 
-    if (lastVisitDate) {
-        var differenceInMs = currentDate - parseInt(lastVisitDate);
-        var differenceInDays = Math.floor(differenceInMs / (1000 * 60 * 60 * 24));
-
-        var message = "";
-        if (differenceInDays === 0) {
-            message = "Back so soon! Awesome!";
-        } else {
-            message = "You last visited " + (differenceInDays === 1 ? "1 day" : differenceInDays + " days") + " ago.";
-        }
-    } else {
-
-        message = "Welcome! Let us know if you have any questions.";
+window.onload = function() {
+    var formLoadedTimestampField = document.getElementById("timestamp");
+    if (formLoadedTimestampField) {
+        formLoadedTimestampField.value = getCurrentTimestamp();
     }
-
-
-    var sidebarContent = document.querySelector("#dateVisit");
-    sidebarContent.textContent = message;
-
-    localStorage.setItem("lastVisitDate", currentDate.toString());
-});
+};
