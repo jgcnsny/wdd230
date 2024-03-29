2f4cb9db204a210d001f7c7aa107069b26e5dc9e
jgcnsny <thegreathappyvirus@gmail.com>
2024-03-29 22:15:44 +0800

    home page

---
+++ scripts/getWeather.js
@@ -0,0 +1,34 @@
+const weatherContainer = document.querySelector('#weather-container')
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=14.19&lon=121.12&units=metric&appid=2da8a4cd6d9da23afaa72a1b524b2798';
+
+async function apiFetch(url) {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            displayResults(data);
+            //console.log(data);
+        } else {
+            throw error(await response.text());
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
+function displayResults(data) {
+    const currentTemp = document.createElement('span');
+    currentTemp.textContent = `${data.main.temp}°C - ${data.weather[0].description}`;
+
+    const weatherIcon = document.createElement('img')
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', 'Weather icon');
+
+    weatherContainer.appendChild(weatherIcon)
+    weatherContainer.appendChild(currentTemp)
+}
+
+apiFetch(url);
---
+++ scripts/links.js
@@ -0,0 +1,29 @@
+const baseURL = 'https://jgcnsny.github.io/wdd230/';
+const linksURL = "https://jgcnsny.github.io/wdd230/data/links.json";
+
+async function getLinks(links) {
+const response = await fetch(links);
+const data = await response.json();
+displayLinks(data.weeks);
+
+}
+
+const displayLinks = (weeks) => {
+    const linksContainer = document.getElementById('links-container');
+  
+    weeks.forEach((week) => {
+        const listElement = document.createElement('li');
+        let weekName = `${week.week}: `;
+        let weekLinksHTML = '';
+
+        week.links.forEach((link) => {
+    
+            weekLinksHTML += `<a href="${link.url}" target="_blank">${link.title}</a>| `;
+        });
+
+        listElement.innerHTML = weekName + weekLinksHTML;
+        linksContainer.appendChild(listElement);
+    });
+};
+ 
+getLinks(linksURL);
